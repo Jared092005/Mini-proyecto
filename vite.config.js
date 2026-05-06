4ea56a405b84dc1ddc304223f5d0b8d88b2e251f
Jared092005 <eliseoborja26@gmail.com>
2026-05-05 21:00:11 -0300

    agregué imagenes de los programas que uso, agregué el tiempo que llevo haciendolo y también hice parte de habilidades adicionales

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+/* import { resolve } from "path"; */
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  /*  build: {
+    rollupOptions: {
+      input: {
+        main: resolveConfig(__dirname, "index.html"),
+      },
+    },
+  }, */
 });
